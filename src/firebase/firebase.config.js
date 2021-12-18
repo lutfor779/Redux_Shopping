67136fc037072e51ed9f54c3369211b8b6d83465
lutfor779 <mdlrrahman779@gmail.com>
2021-12-18 21:15:46 +0600

    .env not working so take another way

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,11 +1,13 @@
+import { REACT_APP_FIREBASE_API_KEY, REACT_APP_FIREBASE_APP_ID, REACT_APP_FIREBASE_AUTH_DOMAIN, REACT_APP_FIREBASE_MEASUREMENT_ID, REACT_APP_FIREBASE_MESSAGING_SENDER_ID, REACT_APP_FIREBASE_PROJECT_ID, REACT_APP_FIREBASE_STORAGE_BUCKET } from "./firebaseConfig";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyD3ufmX4_A3Xwad6YOK_e5MiSCGDmlVjbc",
-    authDomain: "redux-store-36f7b.firebaseapp.com",
-    projectId: "redux-store-36f7b",
-    storageBucket: "redux-store-36f7b.appspot.com",
-    messagingSenderId: "918839203376",
-    appId: "1:918839203376:web:709821ec137d3193237ff5",
-    measurementId: "G-X8JW20JF32"
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID,
+    measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 export default firebaseConfig;
